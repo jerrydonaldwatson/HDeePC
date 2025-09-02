a7c7307ee6c6ddec032de22b8f0a86e82831d248
Jeremy Donald Watson <jeremy.donald.watson@gmail.com>
2025-09-02 16:52:50 +1200

    Updated plots to include system ID + MPC
    Also add more detail to figure.

--- bess_plots.m
+++ bess_plots.m
@@ -21,18 +21,30 @@ load hdeepc
 u_sim_hdpc = u_sim;
 y_sim_hdpc = true_y_sim;
 
+load bess_sid_mpc.mat
+
+u_sim_mpc = u_sim;
+y_sim_mpc = true_y_sim;
+
 figure(2)
+
+
 subplot(3,1,1)
-plot(1:k,u_sim_dpc(1,1:k), 1:k,u_sim_hdpc(1,1:k))
-title('BESS current')
-legend('DeePC', 'HDeePC', 'Location', 'best')
-
+plot(1:k,u_sim_dpc(1,1:k), 1:k,u_sim_hdpc(1,1:k), 1:k,u_sim_mpc(1,1:k))
+title('BESS current (u_1)')
+ylabel('Current (A)')
+%legend('DeePC', 'HDeePC', 'Location', 'best')
+set(gca,'FontSize',12) 
 subplot(3,1,2)
-plot(1:k,y_sim_dpc(1,1:k), 1:k,y_sim_hdpc(1,1:k))
-title('Node voltage deviation')
-legend('DeePC', 'HDeePC', 'Location', 'best')
-
+plot(1:k,y_sim_dpc(1,1:k), 1:k,y_sim_hdpc(1,1:k), 1:k,y_sim_mpc(1,1:k))
+title('Node voltage deviation (x_1)')
+ylabel('Voltage (V)')
+%legend('DeePC', 'HDeePC', 'Location', 'best')
+set(gca,'FontSize',12) 
 subplot(3,1,3)
-plot(1:k,y_sim_dpc(2,1:k), 1:k,y_sim_hdpc(2,1:k), 'LineWidth', 2)
-title('BESS SoC')
-legend('DeePC', 'HDeePC', 'Location', 'best')
+plot(1:k,y_sim_dpc(2,1:k), 1:k,y_sim_hdpc(2,1:k),1:k,y_sim_mpc(2,1:k),  'LineWidth', 2)
+title('BESS SoC (x_3)')
+ylabel('State-of-charge')
+legend('DeePC', 'HDeePC', 'Sys ID + MPC', 'Location', 'best')
+xlabel('Time-step')
+set(gca,'FontSize',12) 
